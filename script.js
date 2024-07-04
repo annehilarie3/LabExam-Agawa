adc05a4632d211d58974371b3b405735b6371224
annehilarie3 <annehilarie.agawa03@gmail.com>
2024-07-04 21:25:25 +0800

    Add

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+document.addEventListener('DOMContentLoaded',
+    function () {
+        const navItems = document
+            .querySelectorAll('.nav-item');
+
+        navItems.forEach(item => {
+            item.addEventListener('click',
+                function () {
+                    navItems.forEach(navItem => navItem
+                        .classList.remove('active'));
+                    this.classList.add('active');
+                });
+        });
+    });
